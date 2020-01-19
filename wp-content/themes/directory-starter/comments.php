492a02bbd259e62b3f2d5a10aab24eabd0fd08df
Florian <flodes80@orange.fr>
2020-01-19 15:45:16 +0100

    Modification design + espace commentaire + single

--- wp-content/themes/directory-starter/comments.php
+++ wp-content/themes/directory-starter/comments.php
@@ -1,12 +1,28 @@
 <a id="comments"></a>
 <?php if($comments) : ?>  
-    <ol class="comments">  
-    <?php foreach($comments as $comment) : ?>  
+    <ol>  
+    <?php $index = 1; foreach($comments as $comment) : ?>  
         <li class="studhelp-comment" id="comment-<?php comment_ID(); ?>" class="<?php if ($comment->user_id == 1) echo "authcomment";?>">      
-            <cite><h3><?php echo get_avatar(get_comment_author_email(), 48, $default_avatar); ?> <?php comment_author_link(); ?> <small>le <?php comment_date(); ?></small></h3></cite><br />
-            <?php comment_text(); ?>  
-        </li>  
-    <?php endforeach; ?>  
+            <cite><h3><?php echo get_avatar(get_comment_author_email(), 36, $default_avatar); ?> <?php comment_author_link(); ?> <small>le <?php comment_date(); ?></small></h3></cite><br />
+            <p>
+				<?php
+					if(($index <= 3 && !in_the_loop()) || in_the_loop()){
+						comment_text(); 
+						$index++;
+					}
+				?>
+			</p>
+        </li>
+		<?php
+			if($index > 3 && !in_the_loop()){
+				break;
+			}
+		?>
+    <?php endforeach;
+		if($index > 3 && !in_the_loop()){
+			echo "<a href='".get_permalink()."'>Voir plus de commentaires ...</a>";
+		}
+	?>
     </ol>  
 <?php endif; ?> 
 <?php if(comments_open()) : ?>
@@ -15,12 +31,11 @@
         <p>You must be <a href="<?php echo get_option('siteurl'); ?>/wp-login.php?redirect_to=<?php echo urlencode(get_permalink()); ?>">logged in</a> to post a comment.</p><?php else : ?>  
         <form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform"> 
             <p><input type="text" name="comment" id="comment"></textarea></p>  
-            <?php //show_subscription_checkbox(); ?>
             <p><input name="submit" type="submit" id="submit" tabindex="5" value="Poster commentaire" />  
             <input type="hidden" name="comment_post_ID" value="<?php echo $id; ?>" /></p>  
             <?php do_action('comment_form', $post->ID); ?>  
         </form>  
     <?php endif; ?>  
 <?php else : ?>  
-    <p>The comments are closed.</p>  
+    <p>Les commentaires sont désactivés.</p>  
 <?php endif; ?>
